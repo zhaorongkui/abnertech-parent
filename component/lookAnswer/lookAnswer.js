c73da0925c1acdba484222ee408f9f3bd75e9e84
赵荣奎 <rkzhao@abnertech.com>
2019-12-28 19:51:49 +0800

    轮播组件

--- component/lookAnswer/lookAnswer.js
+++ component/lookAnswer/lookAnswer.js
@@ -1,17 +1,43 @@
 // component/lookAnswer/lookAnswer.js
 Page({
-
+  onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'page/component/pages/swiper/swiper'
+    }
+  },
   /**
    * 页面的初始数据
    */
   data: {
+    background: ['../../assets/img/jxbg.png', '../../assets/img/jxbg.png', '../../assets/img/jxbg.png'],
+    indicatorDots: false,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
+    current: 0,
+    imgUrl: '777777'
+  },
 
+  // 当滑块切换时触发事件
+  onSliderChangeEnd:function(e) {
+    let that = this;
+    that.setData({
+      current: e.detail.current
+    })
+    console.log(that.data.current)
+  },
+  /* 获取滑块左右滑动时位置 */
+  hotSwiperTransition: function(e) {
+    // console.log(e)
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log('1111111')
 
   },
 
@@ -19,6 +45,8 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    console.log('2222222')
+    console.log(this.data.imgUrl)
 
   },
 
@@ -26,7 +54,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
   },
 
   /**
@@ -64,3 +91,10 @@ Page({
 
   }
 })
+// Component({
+//   lifetimes: {
+//   ready: function() {
+//     console.log(this,'------------------')
+//   }
+//   }
+// })
