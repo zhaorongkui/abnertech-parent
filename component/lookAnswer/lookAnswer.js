24873d3449a4c5ce471e8996521256ed617b9b0c
赵荣奎 <rkzhao@abnertech.com>
2020-01-03 16:07:57 +0800

    看大图实现

--- component/lookAnswer/lookAnswer.js
+++ component/lookAnswer/lookAnswer.js
@@ -1,16 +1,7 @@
 // component/lookAnswer/lookAnswer.js
-Page({
-  onShareAppMessage() {
-    return {
-      title: 'swiper2',
-      path: 'page/component/pages/swiper/swiper'
-    }
-  },
-})
-
 Component({
   properties: {
-    lookres: Object, // 简化的定义方式
+    // lookres: Object, // 简化的定义方式
   },
   /**
    * 组件的初始数据
@@ -23,7 +14,66 @@ Component({
     interval: 2000,
     duration: 500,
     current: 0,
-    infos: [],
+    infos: {
+      "questionId": 4359,
+      "revisionQuestionId": 350,
+      "questionNumber": "3",
+      "workbookChapterId": 2889,
+      "questionSourceId": 1,
+      "questionTypeCode": 6,
+      "subjectAbbreviation": "shuxue",
+      "questionProperty": 1,
+      "workbookName": "（测试勿动）初中数学|练一练",
+      "textbookName": "（勿动！测试）九年级|数学|上册",
+      "revisionStudentAnswer": {
+        "hasAnswer": 1,
+        "submitTime": 1577959804000,
+        "answerState": 2,
+        "answerType": 1,
+        "studentAnswerFiles": [{
+          "fileSort": 1,
+          "answerFileUrl": "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/20200102/16321577959803280unknown.jpg"
+        }]
+      },
+      "studentShareUrlList": [{
+        "studentInfoId": 1651,
+        "urlList": ["http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376186ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376197ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/1661559110383283ABN20171200069.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376205ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376209ABN20171200047.jpg"]
+      }, {
+        "studentInfoId": 1652,
+        "urlList": ["http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376186ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376197ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/1661559110383283ABN20171200069.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376205ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376209ABN20171200047.jpg"]
+      }, {
+        "studentInfoId": 1653,
+        "urlList": ["http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376186ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376197ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/1661559110383283ABN20171200069.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376205ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376209ABN20171200047.jpg"]
+      }, {
+        "studentInfoId": 1654,
+        "urlList": ["http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376186ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376197ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/1661559110383283ABN20171200069.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376205ABN20171200047.jpg", "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/shuxue/20190529/16511559098376209ABN20171200047.jpg"]
+      }],
+      "workbookChapterPage": 1,
+      "questionSourceName": "练习册",
+      "questionYytypeName": "简答题",
+      "revisionId": 71,
+      "studentInfoId": 1655,
+      "questionTypeName": "主观题",
+      "homeworkId": 2761,
+      "questionStem": "<p>某商贩购进一批苹果到集贸市场出售，已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：已知卖出的苹果数量x与售价y的关系如下表：</p>",
+      "homeworkStudentAnswer": {
+        "isTrue": 1,
+        "hasAnswer": 1,
+        "answerState": 2,
+        "answerType": 1,
+        "hasRewive": 1,
+        "studentAnswerFiles": [{
+          "fileSort": 1,
+          "answerFileUrl": "/img/homework/20200102/16321577959751088unknown.jpg",
+          "answerFileUrlStr": "http://lochi-sit.oss-cn-beijing.aliyuncs.com/img/homework/20200102/16321577959751088unknown.jpg"
+        }]
+      },
+      "workbookChapterNameList": ["第一章 有理数"],
+      "questionAnswer": "<p>某商贩购进一批苹果到集贸市场出售，已知卖出的苹果数量x与售价y的关系如下表：</p>",
+      "questionInfoId": 1253,
+      "questionType": 1,
+      "questionAnalysis": "<p>某商贩购进一批苹果到集贸市场出售，已知卖出的苹果数量x与售价y的关系如下表：</p>"
+    },
     parmres: {},
     studentIndex: -1,
     studentShareUrlList: [],
@@ -32,13 +82,30 @@ Component({
     touchS: 0,
     touchE: 0,
     blanks: '',
-    blanksArr: []
+    blanksArr: [],
+    scrollWidth: 0,
+    imgheights: []
   },
   lifetimes: {
+    onShareAppMessage() {
+      return {
+        title: 'swiper2',
+        path: 'page/component/pages/swiper/swiper'
+      }
+    },
     attached() {
       // 在组件实例进入页面节点树时执行
     },
     ready() {
+      var that = this;
+      wx.getSystemInfo({
+        success: function (res) {
+          //获取屏幕的宽度并保存
+          that.setData({
+            scrollWidth: res.windowWidth
+          });
+        }
+      });
       // 在组件在视图层布局完成后执行
       // 父组件传过来的data,用this.data.res接收
       this.setData({
@@ -75,7 +142,7 @@ Component({
       })
       console.log(that.data.current)
     },
-
+    // 滑动开始的坐标
     touchStart: function (e) {
       let sx = e.touches[0].pageX
       this.data.touchS = sx
@@ -84,33 +151,32 @@ Component({
       let sx = e.touches[0].pageX;
       this.data.touchE = sx
     },
+    // 滑动结束的坐标
     touchEnd: function (e) {
       let self = this
       // let start = this.data.touchS
       // let end = this.data.touchE
-      // console.log(start)
-      // console.log(end)
       // 参考答案
       if (
         self.data.studentIndex === -1 &&
-        self.infos.studentShareUrlList.length !== 0 &&
-        self.data.touchS - self.data.touchE > 100
+        self.data.infos.studentShareUrlList.length !== 0 &&
+        self.data.touchS - self.data.touchE > 130
       ) {
         self.shareAnswer(self.data.studentIndex + 1)
       }
 
       if (self.data.studentIndex > -1) {
         let arryLength =
-          self.infos.studentShareUrlList[self.data.studentIndex]
+          self.data.infos.studentShareUrlList[self.data.studentIndex]
           .urlList.length - 1
         // 当最后一个同学不予许再往后滑切换
         if (
           self.data.studentIndex <
-          self.infos.studentShareUrlList.length - 1
+          self.data.infos.studentShareUrlList.length - 1
         ) {
           if (
             self.data.current === arryLength &&
-            self.data.touchS - self.data.touchE > 100
+            self.data.touchS - self.data.touchE > 130
           ) {
             self.shareAnswer(self.data.studentIndex + 1)
           }
@@ -120,7 +186,7 @@ Component({
       if (
         self.data.current === 0 &&
         self.data.studentIndex !== -1 &&
-        self.data.touchE - self.data.touchS > 100
+        self.data.touchE - self.data.touchS > 130
       ) {
         self.shareAnswer(self.data.studentIndex - 1)
       }
@@ -128,37 +194,72 @@ Component({
 
     // 切换分享同学答案按钮，切换轮播
     shareAnswer(index) {
-      console.log(index)
+      var self = this
       if (!index.currentTarget) {
-        var self = this
-        self.data.studentIndex = index
-        this.activeIndex1 = 0
-        index === -1 ? (this.data.subjctShowe = true) : (this.data.subjctShowe = false)
-        this.data.shareBtnIndex = index
+        self.setData({
+          studentIndex: index,
+          shareBtnIndex: index,
+          current: 0
+        })
+        let show = true
+        index === -1 ? (show = true) : (show = false)
+        self.setData({
+          subjctShowe: show
+        })
         if (
           index !== -1 &&
-          index < this.infos.studentShareUrlList.length
+          index < this.data.infos.studentShareUrlList.length
         ) {
-          this.data.studentShareUrlList = this.infos.studentShareUrlList[
-            index
-          ].urlList
+          self.setData({
+            studentShareUrlList: this.data.infos.studentShareUrlList[index].urlList
+          })
+        }
+      } else {
+        var index = index.currentTarget.dataset.index
+        self.setData({
+          studentIndex: index,
+          shareBtnIndex: index,
+          current: 0
+        })
+        let show = true
+        index === -1 ? (show = true) : (show = false)
+        self.setData({
+          subjctShowe: show
+        })
+        if (
+          index !== -1 &&
+          index < self.data.infos.studentShareUrlList.length
+        ) {
+          self.setData({
+            studentShareUrlList: this.data.infos.studentShareUrlList[index].urlList
+          })
         }
       }
-      var self = this
-      var index = index.currentTarget.dataset.index
-      self.data.studentIndex = index
-      this.activeIndex1 = 0
-      index === -1 ? (this.data.subjctShowe = true) : (this.data.subjctShowe = false)
-      this.data.shareBtnIndex = index
-      if (
-        index !== -1 &&
-        index < this.infos.studentShareUrlList.length
-      ) {
-        this.data.studentShareUrlList = this.infos.studentShareUrlList[
-          index
-        ].urlList
-      }
+    },
+    /*** 预览图片****/
+    previewImage: function (e) {
+      var url = e.currentTarget.dataset.src;
+      wx.previewImage({
+        current: url, // 当前显示图片的http链接 
+        urls: this.data.studentShareUrlList // 需要预览的图片http链接列表  
+      })
+    },
+    //等比缩放图片并保存
+    imageLoad: function (e) {
+      //获取图片真实宽度  
+      var imgwidth = e.detail.width,
+        imgheight = e.detail.height,
+        //宽高比  
+        ratio = imgwidth / imgheight;
+      //计算的高度值  
+      var viewHeight = parseInt(this.data.scrollWidth) / ratio;
+      var imgheight = viewHeight.toFixed(0);
+      var imgheightarray = this.data.imgheights;
+      //把每一张图片的高度记录到数组里
+      imgheightarray.push(imgheight);
+      this.setData({
+        imgheights: imgheightarray,
+      });
     }
-
   }
 })
